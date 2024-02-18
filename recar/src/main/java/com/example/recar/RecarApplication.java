abb1e15cbd85cf3653920f7fe962ab330c0589c2
jerry8534 <jerryjjinu@gmail.com>
2024-02-18 12:08:46 +0900

    first commit

---
+++ recar/src/main/java/com/example/recar/RecarApplication.java
@@ -0,0 +1,13 @@
+package com.example.recar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecarApplication.class, args);
+	}
+
+}
---
+++ recar/src/test/java/com/example/recar/RecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
